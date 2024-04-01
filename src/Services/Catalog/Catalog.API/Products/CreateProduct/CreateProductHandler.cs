8cdd08acb64deb30fcd337841695422332f2d490
James Uguna <jamesekene8@gmail.com>
2024-04-01 13:23:29 +0100

    Add Abstraction layer ICommand and IQuery

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,15 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace Catalog.API.Products.CreateProduct
+{
+	public record CreatProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
+	public record CreateProductResult(Guid Id);
+	internal class CreateProductCommandHandler : ICommandHandler<CreatProductCommand, CreateProductResult>
+	{
+		public Task<CreateProductResult> Handle(CreatProductCommand command, CancellationToken cancellationToken)
+		{
+			// Business logic to create a new product
+			throw new NotImplementedException();
+		}
+	}
+}
